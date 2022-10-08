af09ccf4aee37b7d8a026f44a458e0de26c86e05
MiniFrenchBread <103425574+MiniFrenchBread@users.noreply.github.com>
2022-10-08 19:25:58 +0800

    chore: build kv

--- node/build.rs
+++ node/build.rs
@@ -10,4 +10,23 @@ fn main() {
         .unwrap();
 
     println!("build ionian-client with status {}", status);
+    
+    println!("cargo:rerun-if-changed=../ionian-kv");
+
+    let status = Command::new("cargo")
+        .current_dir("../ionian-kv")
+        .args(vec!["build", "--release", "--all-features"])
+        .status()
+        .unwrap();
+
+    println!("build ionian-kv with status {}", status);
+    
+    println!("cargo:rerun-if-changed=../ionian-kv/target/release/ionian-kv");
+    let status = Command::new("cp")
+        .current_dir("../ionian-kv")
+        .args(vec!["target/release/ionian_kv", "../target/"])
+        .status()
+        .unwrap();
+    
+    println!("copy ionian-kv with status {}", status);
 }
